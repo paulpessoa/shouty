7aaf542d5a5950094ae85f4343896dd31942613a
Paul Pessoa <paulmspessoa@gmail.com>
2022-09-28 10:41:14 -0300

    import Gherkin plain language and generate steps

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,16 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+Given('Lucy is located {int} meterss from Sean', function (int) {
+  // Given('Lucy is located {float} meterss from Sean', function (float) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+When('Sean shouts {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+Then('Lucy hears Sean\'s message', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
